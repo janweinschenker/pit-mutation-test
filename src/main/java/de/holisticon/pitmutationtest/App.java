bff3e63e2959878db9bd43d693e8f2cb439e4674
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-11-17 08:34:22 +0100

    chore: banner and manifest

--- src/main/java/de/holisticon/pitmutationtest/App.java
+++ src/main/java/de/holisticon/pitmutationtest/App.java
@@ -1,19 +1,35 @@
 package de.holisticon.pitmutationtest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
  */
+@SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class App {
-  public static void main(String[] args) {
-    Quicksort qs = new Quicksort();
-    List<Integer> integers = qs.sort(Arrays.asList(7, 4, 78, 109, 21, 19, 57));
+  private static final Logger LOG = Logger.getLogger(App.class);
 
-    System.out.println("Hello World!");
+  public static void main(String[] args) {
+    SpringApplication
+        .run(App.class)
+        .close();
   }
 
+  @Bean
+  public CommandLineRunner run() {
+    return args -> {
+      Quicksort qs = new Quicksort();
+      List<Integer> integers = qs.sort(Arrays.asList(7, 4, 78, 109, 21, 19, 57));
+      LOG.info(integers);
+    };
+  }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module de.holisticon.pitmutationtest {
+  requires spring.boot.autoconfigure;
+  requires log4j.over.slf4j;
+  requires spring.context;
+  requires spring.boot;
 
   exports de.holisticon.pitmutationtest;
 }
